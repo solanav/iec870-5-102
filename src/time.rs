bf80264ee659d56f9498d98608dfa0b4d2b21790
solanav <solanav@qq.com>
2020-04-07 10:39:38 +0200

    Added time label structures

--- src/time.rs
+++ src/time.rs
@@ -4,9 +4,35 @@ pub enum TimeLabel {
 }
 
 pub struct TimeLabelA {
-
+    minute: u8,
+    TIS: u8,
+    IV: u8,
+    hour: u8,
+    RES1: u8,
+    SU: u8,
+    month_day: u8,
+    week_dat: u8,
+    month: u8,
+    ETI: u8,
+    PTI: u8,
+    year: u8,
+    RES2: u8,
 }
 
 pub struct TimeLabelB {
-
+    minute: u8,
+    TIS: u8,
+    IV: u8,
+    hour: u8,
+    RES1: u8,
+    SU: u8,
+    month_day: u8,
+    week_dat: u8,
+    month: u8,
+    ETI: u8,
+    PTI: u8,
+    year: u8,
+    RES2: u8,
+    seconds: u8,
+    milliseconds: u8,
 }
