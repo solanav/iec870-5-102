104d899974c824c84e7b7793881f712358d39231
solanav <solanav@qq.com>
2020-04-07 12:33:59 +0200

    DynamicFrame from binary done

--- src/frame.rs
+++ src/frame.rs
@@ -1,11 +1,22 @@
 use crate::time::TimeLabel;
 
-const MAX_FRAME: usize = 255;
+pub const MAX_FRAME: usize = 255;
+const START_FRAME_BYTE: u8 = 0x68;
+const END_BYTE: u8 = 0x16;
 
+#[derive(Debug)]
 pub struct DynamicFrame {
     control: u8,
-    address: u16,
-    asdu: u8,
+    em_address: u16,
+    asdu_type_id: u8,
+    number_obj: u8,
+    cause: u8,
+    measurement_point: u8,
+    record_address: u16,
+    first_integrated: u8,
+    last_integrated: u8,
+    start_time: u64,
+    end_time: u64,
 }
 
 pub struct StaticFrame {
@@ -18,8 +29,16 @@ impl DynamicFrame {
     pub fn new() -> Self{
         DynamicFrame {
             control: 0,
-            address: 0,
-            asdu: 0,
+            em_address: 0,
+            asdu_type_id: 0,
+            number_obj: 0,
+            cause: 0,
+            measurement_point: 0,
+            record_address: 0,
+            first_integrated: 0,
+            last_integrated: 0,
+            start_time: 0,
+            end_time: 0,
         }
     }
 
@@ -35,13 +54,13 @@ impl DynamicFrame {
     ///     3  - user data, FCV = 1
     ///     9  - link state request, FCV = 0
     ///     11 - class 2 data request, FCV = 1
-    pub fn set_control(prm: u8, fcb: u8, fcv: u8, dfc: u8) {
+    pub fn set_control(self, prm: u8, fcb: u8, fcv: u8, dfc: u8) {
 
     }
 
     /// Address
     /// Address from 0x0000 to 0xFFFF unique to the energy meter
-    pub fn set_address(addr: u16) {
+    pub fn set_address(self, addr: u16) {
 
     }
 
@@ -54,7 +73,7 @@ impl DynamicFrame {
     ///     {7}   - test (0 is no test, 1 is test)
     /// {16 bit} mp_addr: 2 bytes of the measurment point address
     /// {8 bit} r_addr: 1 byte of the record address
-    pub fn set_dui(type_id: u8, vsq: u8, cause: u8, mp_addr: u16, r_addr: u8) {
+    pub fn set_dui(self, type_id: u8, vsq: u8, cause: u8, mp_addr: u16, r_addr: u8) {
         if cause >= 128 {
             panic!("Error creating dynamic frame. Cause field's value has to be [0, 127]");
         }
@@ -65,8 +84,66 @@ impl DynamicFrame {
     /// {32 bit} totals: 32 bit number representing energy
     /// {8 bit} qb: qualifier byte for the totals
     /// {x bit} time: time label
-    pub fn set_infobj(addr: u8, totals: u32, qb: u8, time: TimeLabel) {
+    pub fn set_infobj(self, addr: u8, totals: u32, qb: u8, time: TimeLabel) {
 
+    }
+}
+
+impl From<[u8; MAX_FRAME]> for DynamicFrame {
+    fn from(bin: [u8; 255]) -> Self {
+        assert_eq!(bin[0], START_FRAME_BYTE);
+
+        assert_eq!(bin[1], bin[2]);
+        let length = bin[1];
+
+        assert_eq!(bin[3], START_FRAME_BYTE);
+        let control = bin[4];
+        let em_address =
+            ((bin[6] as u16) << 8) +
+            bin[5] as u16;
+        let asdu_type_id = bin[7];
+
+        assert!(bin[8] < 128);
+        let number_obj = bin[8];
+        let cause = bin[9];
+        let measurement_point = bin[10];
+        let record_address =
+            ((bin[12] as u16) << 8) +
+            bin[11] as u16;
+
+        let first_integrated = bin[13];
+        let last_integrated = bin[14];
+
+        let start_time =
+            ((bin[19] as u64) << 32) +
+            ((bin[18] as u64) << 24) +
+            ((bin[17] as u64) << 16) +
+            ((bin[16] as u64) << 8) +
+            bin[15] as u64;
+
+        let end_time =
+            ((bin[24] as u64) << 32) +
+            ((bin[23] as u64) << 24) +
+            ((bin[22] as u64) << 16) +
+            ((bin[21] as u64) << 8) +
+            bin[20] as u64;
+
+        let checksum = bin[25];
+        assert_eq!(bin[26], END_BYTE);
+
+        DynamicFrame {
+            control,
+            em_address,
+            asdu_type_id,
+            number_obj,
+            cause,
+            measurement_point,
+            record_address,
+            first_integrated,
+            last_integrated,
+            start_time,
+            end_time
+        }
     }
 }
 
--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,28 @@ mod time;
 
 #[cfg(test)]
 mod tests {
+    use crate::frame::{MAX_FRAME, DynamicFrame};
+
     #[test]
-    fn it_works() { assert_eq!(2 + 2, 4); }
+    fn asdu_122() {
+        // Create the bin and the data
+        let mut bin= [0u8; MAX_FRAME];
+        let data = [
+            0x68, 0x15, 0x15, 0x68,
+            0x73, 0x58, 0x1B, 0x7A,
+            0x01, 0x06, 0x01, 0x00,
+            0x0B, 0x01, 0x08, 0x00,
+            0x0B, 0x07, 0x02, 0x0A,
+            0x00, 0x11, 0x0A, 0x02,
+            0x0A, 0xC1, 0x16,
+        ];
+
+        // Copy data to binary load
+        for i in 0..data.len() { bin[i] = data[i]; }
+
+        // Try to deserialize the bin
+        let df = DynamicFrame::from(bin);
+
+        println!("{:?}", df);
+    }
 }
